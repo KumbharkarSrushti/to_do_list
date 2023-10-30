678738aded6aacab7fb43843bdd4adfa2c630096
srushti <srushti20@navgurukul.org>
2023-10-30 19:35:33 +0530

    Done and Delete functionality add

--- app/showform.js
+++ app/showform.js
@@ -1,83 +1,81 @@
 "use client"
 import React, { useState } from 'react';
 
-const TodoList=()=>{
-    const [userInput,setUserInput] =useState('')
-    const [todoList,setTodoList] =useState([])
+const TodoList = () => {
+  const [userInput, setUserInput] = useState('');
+  const [todoList, setTodoList] = useState([]);
+  const [TaskAdd, setTaskAdd] = useState(false);
+  const [compTasks, setCompTasks] = useState([]);
 
-    const handleChange=(e)=>{
-        e.preventDefault()
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
 
-        setUserInput(e.target.value)
-        console.log(userInput)
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (userInput.trim() !== '' && !TaskAdd) {
+      setTodoList([...todoList, userInput]);
+      setUserInput('');
+      setTaskAdd(false);
+      setCompTasks([...compTasks, false]); 
     }
-    const handleSubmit =(e)=>{
-        e.preventDefault()
+  };
 
-        setTodoList([
-            userInput,
-            ...todoList
-        ])
-    }
-    return(
-        <div>
-            <form>
-                <input type='text' onChange={handleChange}/><button onClick={handleSubmit}>submit</button>
-            </form>
-            <ul>
-                {
-                    todoList.length >=1? todoList.map((todo,idx)=>{
-                        return <li key={idx}>{todo}</li>
-                    })
-                    :"Enter a todo item"
-                }
-            </ul>
+  const handleTaskClick = (index) => {
+    const updatedTasks = [...compTasks];
+    updatedTasks[index] = !updatedTasks[index];
+    setCompTasks(updatedTasks); 
+  };
 
-        </div>
+  const handleDelete = (index) => {
+    const updatedList = [...todoList];
+    updatedList.splice(index, 1);
+    setTodoList(updatedList);
 
-    )
+    const updatedTasks = [...compTasks];
+    updatedTasks.splice(index, 1);
+    setCompTasks(updatedTasks);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type='text'
+          value={userInput}
+          placeholder='Enter a todo list'
+          onChange={handleChange}
+        />
+        <button type="submit" disabled={TaskAdd}>
+          Add
+        </button>
+      </form>
+      <ul>
+        {todoList.length >= 1 ? (
+          todoList.map((todo, index) => (
+            <li key={index} style={{ textDecoration: compTasks[index] ? 'line-through' : 'none' }}>
+              <span onClick={() => handleTaskClick(index)}>
+                {todo}
+              </span>
+              <button
+                onClick={() => handleDelete(index)}
+              >
+                Delete
+              </button>
+              <button
+                onClick={() => handleTaskClick(index)}
+              >
+                Done
+              </button>
+            </li>
+          ))
+        ) : (
+          <p>Enter a todo item</p>
+        )}
+      </ul>
+    </div>
+  );
 }
-
-
-
-
-
-
-// function TodoList() {
-//   const [task, setTask] = useState('');
-//   const [tasks, setTasks] = useState([]);
-
-//   const handleInputChange = (e) => {
-//     setTask(e.target.value);
-//   };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (task.trim() !== '') {
-//       setTasks([...tasks, task]);
-//       setTask('');
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>To-Do List</h1>
-//       <form onSubmit={handleSubmit}>
-//         <input
-//           type="text"
-//           placeholder="Add a new task"
-//           value={task}
-//           onChange={handleInputChange}
-//         />
-//         <button type="submit">Add</button>
-//       </form>
-//       <ul>
-//         {tasks.map((task, index) => (
-//           <li key={index}>{task}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// }
 
 export default TodoList;
